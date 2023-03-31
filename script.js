6172d3cba6697ceb6f6259be636e3c3c99f46df6
Rahul Sharma <102500734+ImRkofficial@users.noreply.github.com>
2023-03-31 18:15:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,14 +28,7 @@ p.then((value1) => {
 })
 
 
-window.onload = function () {
-	// window.addEventListener('scroll', function (e) {
-	// 	if (window.pageYOffset > 100) {
-	// 		document.querySelector("header").classList.add('is-scrolling');
-	// 	} else {
-	// 		document.querySelector("header").classList.remove('is-scrolling');
-	// 	}
-	// });
+window.onload = function () {
 
 	const menu_btn = document.querySelector('.hamburger');
 	const mobile_menu = document.querySelector('.mobile-nav');
